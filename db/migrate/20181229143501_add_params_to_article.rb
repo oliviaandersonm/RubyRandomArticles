ea481ebd23e560b17127974afea77efb648e09be
Olivia Anderson <oanders@tulane.edu>
2019-01-06 16:57:17 +0000

    update migration versions

--- db/migrate/20181229143501_add_params_to_article.rb
+++ db/migrate/20181229143501_add_params_to_article.rb
@@ -1,4 +1,4 @@
-class AddParamsToArticle < ActiveRecord::Migration
+class AddParamsToArticle < ActiveRecord::Migration[5.2.2]
   def change
     add_column :articles, :title, :string
     add_column :articles, :author, :string
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181229143501) do
+ActiveRecord::Schema.define(version: 2018_12_29_143501) do
 
   create_table "articles", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "title"
-    t.string   "author"
-    t.string   "source"
-    t.string   "description"
-    t.string   "url"
-    t.string   "image"
-    t.string   "pub_date"
-    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "author"
+    t.string "source"
+    t.string "description"
+    t.string "url"
+    t.string "image"
+    t.string "pub_date"
+    t.string "content"
   end
 
 end
