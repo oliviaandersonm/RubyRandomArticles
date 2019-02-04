61c766cde68488979a01740b4a64f97540cc6435
Olivia Anderson <oanders@tulane.edu>
2019-02-04 02:18:09 +0000

    scroller for articles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,28 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function shiftSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {
+      slideIndex = 1
+  } 
+  if (n < 1) {
+      slideIndex = slideIndex - 1;
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slides[slideIndex].style.display = "inline"; 
+  slides[slideIndex+1].style.display = "inline";
+  slides[slideIndex+2].style.display = "inline";
+}
+
